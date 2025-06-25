8b0b144bf09224402d2f9f8ef27bbcdab112e81c
ncmoliver <mrmarquesoliver@gmail.com>
2025-06-25 14:31:45 -0400

    5. Detailed view udpated with image

--- feed/urls.py
+++ feed/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, PostDetailview
 
 app_name = 'feed'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='index'),
+    path('detail/<int:pk>/', PostDetailview.as_view(), name='detail'),
 ]
